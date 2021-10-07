6ef8afcf05edf1d5e29c2833e01173245371751c
110318 <andrescamilogomezamaya@gmail.com>
2021-10-06 21:44:52 -0500

    Nuevos ejemplos añadidos

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,46 @@ const user = {
 
 user.location;
 
+
+const anuncio = "hello world";
+
+anuncios.toLocaleLowerCase();
+anuncio.toLocaleLowerCase();
+
+function flipCoin(){
+
+    return Math.random () < 0.5;
+
+}
+
+function rect(number: number, number2: number, number3: number, number4: number) {
+
+}
+
+const coin = rect(20,20,20,20);
+coin.flipCoin();
+
+const value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+    // ...
+} else if (value === "b") {
+}
+// Solución
+// El ejemplo anterior da un error porque tanto "a" como "b" son falsos.
+
+const value1 = Math.random() < 0.5 ? "a" : "b";
+if (value1 == "a") {
+    // ...
+} else if (value1 === "b") {
+}
+
+// Duda
+const value2 = Math.random() < 0.5 ? "a" : "b";
+if (value2 !== "a") {
+    // ...
+} else if (value2 == "b") {
+}
+
+
+
+
